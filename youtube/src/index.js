767a4b120f152afcf3c9b2e81b4479a6122087b2
mkfdev <rosy.fdev@gmail.com>
2021-08-12 14:51:22 +0900

    add youtube service

--- youtube/src/index.js
+++ youtube/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import Youtube from './service/youtube';
 
+//index.js가 호출되는 단 한번만 호출함
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
